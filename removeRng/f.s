06d6df3935d44fec5b8100e195a2bf2b6654b6fa
Kamil Marszalek <kamilmar553@gmail.com>
2024-05-15 11:42:42 +0200

    Remove rng

---
+++ removeRng/f.s
@@ -0,0 +1,32 @@
+section .text
+global f
+f:
+    push    ebp
+    mov     ebp, esp
+    push    esi
+    push    edi
+    mov     esi, [ebp+8]
+    mov     cl, byte [ebp+12]
+    mov     dl, byte [ebp+16]
+    mov     edi, esi
+begin:  
+    mov     al, [esi]
+    cmp     al, 0
+    jz      end
+    cmp     al, cl
+    jl      save
+    cmp     al, dl
+    jle      next
+save:
+    mov     [edi], al
+    inc     edi
+next:
+    inc     esi
+    jmp     begin
+end:
+    mov     byte[edi], 0
+    pop     edi
+    pop     esi
+    pop     ebp
+    ret
+    
---
+++ removeRng/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "f.h"
+int main(int argc, char *argv[]){
+    if(4>argc){
+        printf("Arg.missing\n");
+        return -1;
+    }
+    f(argv[1], *argv[2], *argv[3]);
+    printf("%s\n", argv[1]);
+    return 0;
+}
