762eb99752ce2e146d20db1c9bd197eff64735cb
Kamil Marszalek <kamilmar553@gmail.com>
2024-05-19 15:12:24 +0200

    Reverse dig

---
+++ reverseDig/f.s
@@ -0,0 +1,51 @@
+; Reverse the order of digits, leaving the other chars in their places.
+
+section .text
+global f
+f:
+    push ebp
+    mov ebp, esp
+    push ebx
+    mov eax, [ebp + 8]
+find_end:
+    mov cl, [eax]
+    cmp cl, 0
+    jz save_end
+    inc eax
+    jmp find_end
+save_end:
+    mov ebx, eax
+    mov eax, [ebp + 8]
+begin:
+    mov cl, [eax]
+    cmp cl, 0
+    jz end
+    cmp eax, ebx
+    jz end
+    cmp cl, '0'
+    jl next
+    cmp cl, '9'
+    jg next
+inner:
+    mov ch, [ebx]
+    cmp ebx, eax
+    jz end
+    cmp ch, '0'
+    jl before
+    cmp ch, '9'
+    jg before
+swap:
+    mov [eax], ch
+    mov [ebx], cl
+    dec ebx
+next:
+    inc eax
+    jmp begin
+before:
+    dec ebx
+    jmp inner
+end:
+    pop ebx
+    mov esp, ebp
+    pop ebp
+    ret
